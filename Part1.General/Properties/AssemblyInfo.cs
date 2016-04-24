70dd4fb6dbaefef39a7a9d232f377392322b6248
Sam Bledsoe <sam.bledsoe@gmail.com>
2016-04-23 20:22:16 -0500

    Step 12

--- Part1.General/Properties/AssemblyInfo.cs
+++ Part1.General/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Part1.General")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Libraries for representing patients and their insurance policies")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sam Bledsoe")]
 [assembly: AssemblyProduct("Part1.General")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
